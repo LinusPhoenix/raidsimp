707cc0555289a5d8b20ebdd3eaef6b6bb0e2c40a
Kári Frederiksen <karifrederiksen@gmail.com>
2021-07-27 18:02:28 +0200

    setup routing, add a frame, initialize some pages

---
+++ frontend/src/pages/RaidTeams.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Typography, Container } from "@material-ui/core";
+
+export function RaidTeamsPage() {
+    return (
+        <Container maxWidth="lg">
+            <Typography>TODO</Typography>
+        </Container>
+    );
+}
---
+++ frontend/src/pages/routes.ts
@@ -0,0 +1,11 @@
+export function home() {
+    return "/";
+}
+
+export function raidTeams() {
+    return "/raid-teams";
+}
+
+export function raidTeam(id: string) {
+    return "/raid-teams/" + id;
+}
