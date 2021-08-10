a8a10307bf5d156b07f44b068ccf38859b4c2066
Linus Desktop <linus.eckhard@gmail.com>
2021-08-10 23:52:01 +0200

    Add more properties to search

--- backend/src/search/dto/search-result.dto.ts
+++ backend/src/search/dto/search-result.dto.ts
@@ -5,6 +5,9 @@ export class SearchResultDto {
         if (options) {
             this.characterName = options.characterName;
             this.realmName = options.realmName;
+            this.className = options.className;
+            this.guildName = options.guildName;
+            this.characterLevel = options.characterlevel;
         }
     }
 
@@ -13,9 +16,21 @@ export class SearchResultDto {
 
     @ApiProperty({ maxLength: 128 })
     realmName: string;
+
+    @ApiProperty({ maxLength: 16 })
+    className: string;
+
+    @ApiProperty({ maxLength: 24 })
+    guildName: string;
+
+    @ApiProperty()
+    characterLevel: number;
 }
 
 export interface SearchResultDtoOptions {
     characterName: string;
     realmName: string;
+    className: string;
+    guildName: string;
+    characterlevel: number;
 }
