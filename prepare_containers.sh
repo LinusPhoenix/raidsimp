01aeba22e02c5e1f7dfaa3174c83dc0bc5ba8d1e
Linus Desktop <linus.eckhard@gmail.com>
2022-02-15 22:34:01 +0100

    Add prepare_containers.sh

---
+++ prepare_containers.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo 'prepare_containers.sh $version'
+    exit 1
+fi
+
+version=$1
+set -x
+
+cd backend
+docker build -t raidsimp-backend:$version
+docker save raidsimp-backend:$version -o ../docker/raidsimp-backend-$version.tar
+
+cd frontend
+docker build -t raidsimp-frontend:$version
+docker save raidsimp-frontend:$version -o ../docker/raidsimp-frontend-$version.tar
