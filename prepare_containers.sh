bbc3d502fb9e804b81eca20c872626e4435c5c10
Linus Desktop <linus.eckhard@gmail.com>
2022-02-16 22:35:29 +0100

    Build fixes

--- prepare_containers.sh
+++ prepare_containers.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 if [ "$#" -ne 1 ]; then
     echo 'prepare_containers.sh $version'
     exit 1
@@ -8,9 +8,9 @@ version=$1
 set -x
 
 cd backend
-docker build -t raidsimp-backend:$version
+docker build -t raidsimp-backend:$version .
 docker save raidsimp-backend:$version -o ../docker/raidsimp-backend-$version.tar
 
-cd frontend
-docker build -t raidsimp-frontend:$version
+cd ../frontend
+docker build -t raidsimp-frontend:$version .
 docker save raidsimp-frontend:$version -o ../docker/raidsimp-frontend-$version.tar
