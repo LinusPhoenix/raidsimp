3fff100ae17309357984c9d731f35dbf632708b7
Kari Frederiksen <karifrederiksen@gmail.com>
2022-02-17 16:27:13 +0100

    #20 Fix footer design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ import InvertColorsIcon from "@material-ui/icons/InvertColors";
 import { useThemeToggle } from "./Theming";
 import { LogInPage } from "./pages/LogIn";
 import { UserInfo } from "./components/UserInfo";
-import { Footer } from "./components/Footer";
+import { Footer, FOOTER_HEIGHT } from "./components/Footer";
 import { PrivacyPage } from "./pages/Privacy";
 
 const HomePage = React.lazy(() => import("./pages/Home"));
@@ -28,7 +28,7 @@ const RaidTeamPage = React.lazy(() => import("./pages/RaidTeam"));
 const Main = styled("main")(({ theme }) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
-    minHeight: "100vh",
+    minHeight: `calc(100vh - ${FOOTER_HEIGHT})`,
     maxHeight: "100%",
     overflow: "auto",
 }));
@@ -52,86 +52,90 @@ const ToolbarTitle = styled(Typography)(() => ({
 }));
 
 export function App() {
-    const showThemeToggle = process.env.NODE_ENV === "development";
-    const toggleTheme = useThemeToggle();
-
     return (
         <Router>
             <CssBaseline />
             <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <AppBar position="fixed">
-                    <Toolbar>
-                        <ToolbarRootLink to="/">
-                            <img
-                                alt="RaidSIMP Icon"
-                                src="/eye_logo.png"
-                                style={{
-                                    objectFit: "contain",
-                                    maxHeight: 64,
-                                }}
-                            />
-                            <ToolbarTitle variant="h5" color="textPrimary" gutterBottom={false}>
-                                RaidSIMP
-                            </ToolbarTitle>
-                        </ToolbarRootLink>
-
-                        <Stack direction={"row"} alignItems={"center"} spacing={3}>
-                            {showThemeToggle && (
-                                <IconButton
-                                    onClick={toggleTheme}
-                                    color="inherit"
-                                    title="Toggle theme"
-                                >
-                                    <InvertColorsIcon />
-                                </IconButton>
-                            )}
-
-                            <React.Suspense fallback={<></>}>
-                                <UserInfo />
-                            </React.Suspense>
-                        </Stack>
-                    </Toolbar>
-                </AppBar>
+                <Header />
                 <Main>
                     <Toolbar />
                     <React.Suspense fallback={<PageLoading />}>
-                        <Switch>
-                            <Route exact path="/">
-                                <HomePage />
-                            </Route>
-                            <Route
-                                path="/raid-teams/:teamId/raiders/:raiderId"
-                                render={({ match }) => (
-                                    <RaiderPage
-                                        teamId={match.params.teamId}
-                                        raiderId={match.params.raiderId}
-                                    />
-                                )}
-                            />
-                            <Route
-                                path="/raid-teams/:teamId"
-                                render={({ match }) => (
-                                    <RaidTeamPage teamId={match.params.teamId} />
-                                )}
-                            />
-                            <Route path="/raid-teams">
-                                <RaidTeamsPage />
-                            </Route>
-                            <Route path="/login">
-                                <LogInPage />
-                            </Route>
-                            <Route path="/privacy">
-                                <PrivacyPage />
-                            </Route>
-                            <Route path="*">
-                                <NotFoundPage />
-                            </Route>
-                        </Switch>
+                        <SwitchAndRoutes />
                     </React.Suspense>
                 </Main>
-                <Footer />
+                <footer>
+                    <Footer />
+                </footer>
             </Box>
         </Router>
+    );
+}
+
+function Header() {
+    const showThemeToggle = process.env.NODE_ENV === "development";
+    const toggleTheme = useThemeToggle();
+
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                <ToolbarRootLink to="/">
+                    <img
+                        alt="RaidSIMP Icon"
+                        src="/eye_logo.png"
+                        style={{
+                            objectFit: "contain",
+                            maxHeight: 64,
+                        }}
+                    />
+                    <ToolbarTitle variant="h5" color="textPrimary" gutterBottom={false}>
+                        RaidSIMP
+                    </ToolbarTitle>
+                </ToolbarRootLink>
+
+                <Stack direction={"row"} alignItems={"center"} spacing={3}>
+                    {showThemeToggle && (
+                        <IconButton onClick={toggleTheme} color="inherit" title="Toggle theme">
+                            <InvertColorsIcon />
+                        </IconButton>
+                    )}
+                    <React.Suspense fallback={<></>}>
+                        <UserInfo />
+                    </React.Suspense>
+                </Stack>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+function SwitchAndRoutes() {
+    return (
+        <Switch>
+            <Route exact path="/">
+                <HomePage />
+            </Route>
+            <Route
+                path="/raid-teams/:teamId/raiders/:raiderId"
+                render={({ match }) => (
+                    <RaiderPage teamId={match.params.teamId} raiderId={match.params.raiderId} />
+                )}
+            />
+            <Route
+                path="/raid-teams/:teamId"
+                render={({ match }) => <RaidTeamPage teamId={match.params.teamId} />}
+            />
+            <Route path="/raid-teams">
+                <RaidTeamsPage />
+            </Route>
+            <Route path="/login">
+                <LogInPage />
+            </Route>
+            <Route path="/privacy">
+                <PrivacyPage />
+            </Route>
+            <Route path="*">
+                <NotFoundPage />
+            </Route>
+        </Switch>
     );
 }
 
