5bdcc9c6fb4c3c4d20a8f1366d20dec3e9b4cf4d
Kári Frederiksen <karifrederiksen@gmail.com>
2022-02-26 09:41:39 +0100

    make test more correct

--- frontend/src/utility/usePromise.test.tsx
+++ frontend/src/utility/usePromise.test.tsx
@@ -1,8 +1,8 @@
-import { render, waitFor } from "@testing-library/react";
+import { render, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import { Suspense } from "react";
 import { PromiseProvider, usePromise } from "./usePromise";
 
-test("allows multiple promises, given they have different keys", async () => {
+test("allows multiple usePromises, given they have different keys", async () => {
     function ImmediateResolve({ n }: { n: number }) {
         const x = usePromise("test " + n, () => Promise.resolve(n), [n]);
         return <div>n={x.data}</div>;
@@ -10,18 +10,16 @@ test("allows multiple promises, given they have different keys", async () => {
 
     const { getByText } = render(
         <PromiseProvider>
-            <Suspense fallback={"loading..."}>
+            <Suspense fallback={<div>loading...</div>}>
                 <ImmediateResolve n={3} />
                 <ImmediateResolve n={2} />
             </Suspense>
         </PromiseProvider>,
     );
+    await waitForElementToBeRemoved(getByText("loading..."));
 
-    const textElem1 = await waitFor(() => getByText("n=3"));
-    expect(textElem1).toBeInTheDocument();
-
-    const textElem2 = await waitFor(() => getByText("n=2"));
-    expect(textElem2).toBeInTheDocument();
+    expect(getByText("n=3")).toBeInTheDocument();
+    expect(getByText("n=2")).toBeInTheDocument();
 });
 
 test("updates when dependencies change", async () => {
