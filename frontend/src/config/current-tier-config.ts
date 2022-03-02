d6b7a1203e50663cb96d88b2f7e1640adf1f891d
Linus Eckhard <linus.eckhard@googlemail.com>
2022-03-02 18:49:52 +0100

    Release 0.2.4 (#26)
    - Change ilvl colors for sepulcher of the first ones

--- frontend/src/config/current-tier-config.ts
+++ frontend/src/config/current-tier-config.ts
@@ -1,7 +1,7 @@
 export class CurrentTierConfiguration {
-    public static readonly TopMythicIlvl: number = 259;
-    public static readonly MythicIlvl: number = 252;
-    public static readonly TopHeroicIlvl: number = 246;
-    public static readonly HeroicIlvl: number = 239;
-    public static readonly NormalIlvl: number = 226;
+    public static readonly TopMythicIlvl: number = 285;
+    public static readonly MythicIlvl: number = 278;
+    public static readonly TopHeroicIlvl: number = 272;
+    public static readonly HeroicIlvl: number = 265;
+    public static readonly NormalIlvl: number = 252;
 }
