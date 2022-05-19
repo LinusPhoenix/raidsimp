81f1da180b2c6542ee0752b6f27c558f66ce4f09
Linus Desktop <linus.eckhard@gmail.com>
2022-05-19 11:57:05 +0200

    Fixes post-merge (wtf happened?)

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Toolbar as MuiToolbar, AppBar, Typography, Stack } from "@material-ui/core";
+import {
+    Toolbar as MuiToolbar,
+    AppBar,
+    Typography,
+    Stack,
+    useTheme,
+    useScrollTrigger,
+} from "@material-ui/core";
 import { styled } from "@material-ui/core/styles";
 import { Link } from "./Link";
 import { ErrorBoundary } from "./ErrorBoundary";
@@ -34,8 +41,19 @@ export function AppHeaderSpace() {
 }
 
 export function AppHeader(props: AppHeaderProps) {
+    const theme = useTheme();
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 25,
+    });
+
     return (
-        <AppBar position="fixed">
+        <AppBar
+            position="fixed"
+            elevation={trigger ? 12 : 0}
+            sx={{ bgcolor: theme.palette.background.default }}
+        >
             <Toolbar>
                 <Stack direction="row" alignItems="center" spacing={3}>
                     <ToolbarRootLink to="/">
