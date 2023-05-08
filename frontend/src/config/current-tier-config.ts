67459d4ba093ef55725fa299d04e66c995a7a2b2
Linus Desktop <linus.eckhard@gmail.com>
2023-05-08 10:36:23 +0200

    Update item levels for wow patch 10.1

--- frontend/src/config/current-tier-config.ts
+++ frontend/src/config/current-tier-config.ts
@@ -1,7 +1,7 @@
 export class CurrentTierConfiguration {
-    public static readonly TopMythicIlvl: number = 424;
-    public static readonly MythicIlvl: number = 415;
-    public static readonly TopHeroicIlvl: number = 411;
-    public static readonly HeroicIlvl: number = 402;
-    public static readonly NormalIlvl: number = 389;
+    public static readonly TopMythicIlvl: number = 450;
+    public static readonly MythicIlvl: number = 441;
+    public static readonly TopHeroicIlvl: number = 437;
+    public static readonly HeroicIlvl: number = 428;
+    public static readonly NormalIlvl: number = 415;
 }
