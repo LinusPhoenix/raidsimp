b23a606773adb842f47235851c21b64d18312a28
Linus Eckhard <linus.eckhard@gmail.com>
2024-08-11 17:19:45 +0200

    chore: update tier config for tww season 1

--- frontend/src/config/current-tier-config.ts
+++ frontend/src/config/current-tier-config.ts
@@ -1,7 +1,7 @@
 export class CurrentTierConfiguration {
-    public static readonly ArtifactIlvl: number = 486;
-    public static readonly LegendaryIlvl: number = 480;
-    public static readonly EpicIlvl: number = 467;
-    public static readonly RareIlvl: number = 454;
-    public static readonly UncommonIlvl: number = 441;
+    public static readonly ArtifactIlvl: number = 639;
+    public static readonly LegendaryIlvl: number = 632;
+    public static readonly EpicIlvl: number = 623;
+    public static readonly RareIlvl: number = 613;
+    public static readonly UncommonIlvl: number = 600;
 }
